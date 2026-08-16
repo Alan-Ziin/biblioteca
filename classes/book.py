c30fb9f9f4b2e8f4d01c15a830f9d2744cf6f16e
Alan Sales <alansales2018@gmail.com>
2026-08-16 18:30:26 -0300

    add news functions in main

--- classes/book.py
+++ classes/book.py
@@ -3,3 +3,6 @@ class Book:
         self.titulo = titulo
         self.autor = autor
         self.ano_publicacao = ano_publicacao
+
+    def __eq__(self, value):
+        return self.titulo == value.titulo and self.autor == value.autor and self.ano_publicacao == value.ano_publicacao
--- classes/library.py
+++ classes/library.py
@@ -2,9 +2,34 @@ class Library:
     def __init__(self):
         self.books = []
 
-    def add_books(self, livro):
-        self.books.append(livro)
+    def add_books(self, book):
+        for duplicate in self.books:
+            if duplicate == book:
+                return print("Erro, o livro já existe\n")
+        self.books.append(book)
+
 
     def show_books(self):
-        for l in self.books:
-            print(f"Name of Book: {l.titulo}\nActor of Book: {l.autor}\nPublication year of Book: {l.ano_publicacao}")
+        if len(self.books) == 0:
+            return print("The bookcase is empty\n")
+        else:
+            print("Books in bookcase")
+            for l in self.books:
+                print(f"Name of Book: {l.titulo}\tActor of Book: {l.autor}\tPublication year of Book: {l.ano_publicacao}\n")
+
+            
+    def remove_books(self, book):
+        for t in self.books:
+            if t == book:
+                print(f"#################\nRemoving {book.titulo}\n#################\n")
+                self.books.remove(t)
+
+
+    def len_bookcase(self):
+        result = len(self.books)
+        if result == 0:
+            print("The bookcase is empty\n")
+            return result
+        else:
+            print(f"Have {result} books in bookcase.\n")
+            return result
