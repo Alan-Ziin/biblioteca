c874bf88fbb76d389b8b2bcd9e2ac875000ff78d
Alan Sales <alansales2018@gmail.com>
2026-08-21 23:58:11 -0300

    update validate fields

--- app.py
+++ app.py
@@ -4,14 +4,28 @@ import streamlit as st
 if "bookcase" not in st.session_state:
     st.session_state["bookcase"] = Library()
 
+if "error" not in st.session_state:
+    st.session_state["error"] = False
+
 bookcase = st.session_state["bookcase"]
 
 name_book = st.text_input("Name of book: ", key="titleText")
 actor_of_book = st.text_input("Actor and writer of book: ", key="actorText")
 year_publication = st.text_input("Year of publication of book: ", key="yearNumber")
 idOfBook = st.text_input("Id of Book: ", key="idNumber")
+textsInputs = [name_book, actor_of_book, year_publication, idOfBook]
 
 def buttonSubmit():
+    st.session_state['error'] = False
+    for i in textsInputs:
+        if i == "":
+            st.session_state['error'] = "Error, please filled the forms"
+            return
+    try:
+       int(year_publication)
+    except:
+        st.session_state['error'] = "Error, please write just number in Year"
+        
     bookCreated = Book(name_book,actor_of_book,year_publication,idOfBook)
     bookcase.add_books(bookCreated)
     st.session_state["titleText"] = ""
@@ -21,5 +35,8 @@ def buttonSubmit():
 
 buttonForms = st.button("Confirm the Forms", on_click=buttonSubmit)
 
-for i in bookcase.books:
-    st.text(str(i))
+if st.session_state['error'] is not "":
+    st.write(st.session_state['error'])
+else:
+    for i in bookcase.books:
+        st.text(str(i))
